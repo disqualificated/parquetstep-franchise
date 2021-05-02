2b2e1a0c424c8024c89003ad34ba9301e69bf0e2
Rodion Rostovschikov <rodionrostovschikov@icloud.com>
2021-05-02 21:15:55 +0300

    modal & components

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,38 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
 "use strict";
+
+var btns = document.querySelectorAll(".btn");
+var modalOverlay = document.querySelector(".modal-overlay ");
+var modals = document.querySelectorAll(".modal");
+var close = document.querySelector(".modal-close");
+
+function removeOverlay() {
+  modalOverlay.classList.remove("modal-overlay--visible");
+}
+
+function closeModal() {
+  modals.forEach(function (el) {
+    el.classList.remove("modal--visible");
+    document.querySelector("body").style.overflow = "visible";
+  });
+}
+
+btns.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    var path = e.currentTarget.getAttribute("data-path");
+    closeModal();
+    document.querySelector("[data-target=\"".concat(path, "\"]")).classList.add("modal--visible");
+    modalOverlay.classList.add("modal-overlay--visible");
+    document.querySelector("body").style.overflow = "hidden";
+  });
+});
+modalOverlay.addEventListener("click", function (e) {
+  if (e.target == modalOverlay) {
+    removeOverlay();
+    closeModal();
+  }
+});
+close.addEventListener("click", function (e) {
+  removeOverlay();
+  closeModal();
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,40 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+const btns = document.querySelectorAll(".btn");
+const modalOverlay = document.querySelector(".modal-overlay ");
+const modals = document.querySelectorAll(".modal");
+const close = document.querySelector(".modal-close");
+
+function removeOverlay() {
+  modalOverlay.classList.remove("modal-overlay--visible");
+}
+function closeModal() {
+  modals.forEach((el) => {
+    el.classList.remove("modal--visible");
+    document.querySelector("body").style.overflow = "visible";
+  });
+}
+
+btns.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    let path = e.currentTarget.getAttribute("data-path");
+
+    closeModal();
+
+    document
+      .querySelector(`[data-target="${path}"]`)
+      .classList.add("modal--visible");
+    modalOverlay.classList.add("modal-overlay--visible");
+    document.querySelector("body").style.overflow = "hidden";
+  });
+});
+
+modalOverlay.addEventListener("click", (e) => {
+  if (e.target == modalOverlay) {
+    removeOverlay();
+    closeModal();
+  }
+});
+
+close.addEventListener("click", (e) => {
+  removeOverlay();
+  closeModal();
+});
